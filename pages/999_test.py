8a331ad009965e4f3467e12c74400370732d4d8f
vkkkv <104302767+VKKKV@users.noreply.github.com>
2024-01-04 19:56:18 +0800

    asd

--- pages/999_test.py
+++ pages/999_test.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
-def method(n,a,b,c):
-    # 汉诺塔
-    if(n == 1):
-        st.write(a,"->",c)
-        return 
-    method(n-1, a, c, b)
-    method(1, a, b, c)
-    method(n-1, b, a, c)
+# def method(n,a,b,c):
+#     # 汉诺塔
+#     if(n == 1):
+#         st.write(a,"->",c)
+#         return 
+#     method(n-1, a, c, b)
+#     method(1, a, b, c)
+#     method(n-1, b, a, c)
 
 
 
@@ -16,7 +16,7 @@ st.title("test")
 text = st.text_area("输入文本")
 # shift = st.number_input("输入位移量", min_value=1, max_value=25, value=3, step=1)
 
-method(int(text), "a", "b", "c")
+# method(int(text), "a", "b", "c")
 
 
 # if st.button("b1"):
@@ -25,4 +25,3 @@ method(int(text), "a", "b", "c")
     # else:
     #     st.success("s2: ")
     #     st.code(method(text))
-
